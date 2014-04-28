efd8b2dafef3a1dc26b118ea83f5c5fe3455a15e
angela ebirim <angelaebirim@Jacks-MacBook-Air-2.local>
2014-04-28 12:12:50 +0100

    github feature now working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-	def self.create_with_omniauth(auth)  
-    create! do |user|  
-      user.provider = auth["provider"]  
-      user.uid = auth["uid"]  
-      user.name = auth["name"]  
-      user.nickname = auth["info"]["nickname"]
-       end
-    end  
+	def self.create_with_omniauth(auth)
+		#require 'pry'; binding.pry
+		create! do |user|  
+			user.provider = auth["provider"]  
+			user.uid = auth["uid"]  
+			user.name = auth["name"]  
+			user.nickname = auth["info"]["nickname"]
+			user.image = auth["info"]["image"]
+		end
+	end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140427100157) do
+ActiveRecord::Schema.define(version: 20140428090152) do
 
   create_table "users", force: true do |t|
     t.string   "provider"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140427100157) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "image"
   end
 
 end
