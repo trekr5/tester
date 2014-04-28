11944ee20a36192d7472b77863a63492b3031e00
angela ebirim <angelaebirim@Jacks-MacBook-Air-2.local>
2014-04-28 18:17:40 +0100

    unit RSpec test on User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,34 @@
 
 require 'spec_helper'
 
-describe "User" do
+describe User do
+
+ describe ".create_with_omniauth" do
+
+        it "sets the nickname" do
 
 
+        end
 
+        it "sets the uid" do
+
+        end
+        
+       
+        it "sets the name"  do 
+
+        end
+
+         it "sets the provider"  do 
+
+        end
+
+         it "sets the image"  do 
+
+        end
+        
+
+
+  end
 
 end	
