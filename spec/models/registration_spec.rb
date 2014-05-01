830f9ac447bc41c2f084a30dc6ce70b2c37217b0
angela ebirim <angelaebirim@Jacks-MacBook-Air-2.local>
2014-05-01 18:55:16 +0100

    unit tests for registration model

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Registration do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+
+  	it "is valid with a user_id and talk_id" do
+  		registration = Registration.new(
+  		 	user_id: '1', talk_id: '1')
+
+  	    expect(registration).to be_valid	 
+  	end
+
+  	
+
+
+    it "is invalid without a user_id and talk_id" do
+
+    	registration = Registration.new(
+  		 	user_id: nil, talk_id: nil)
+
+       expect(registration).should_not be_valid
+
+    end 	
+
 end
