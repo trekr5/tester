98c2f4a1a8d57b71bf7198981a35dd166f61a2c8
Angela Ebirim <cebirim@gmail.com>
2014-05-18 17:50:55 +0100

    schedule controller test

--- spec/controllers/schedule_controller_spec.rb
+++ spec/controllers/schedule_controller_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe ScheduleController do
 
+  it "renders the :index view" do
+  get :index 
+
+  	response.should render_template :index
+   
+  end	
 end
--- spec/controllers/whitepaper_controller_spec.rb
+++ spec/controllers/whitepaper_controller_spec.rb
@@ -2,4 +2,26 @@ require 'spec_helper'
 
 describe WhitepaperController do
 
+	#let(pdf)  {filename: "test.pdf", disposition: "attachment", type: "application/pdf"}
+	#reader = PDF::Reader.new("test.pdf")
+
+	describe "#readpdf" do
+
+		let(:pdf) {{disposition: "attachment", filename: "test.pdf", type: "application/pdf"}}
+
+	   it "should download a pdf" do
+	     
+        controller.stub(:render)
+        controller.should_receive(:send_file)
+        get :readpdf, :name => "test.pdf"
+        controller.response.header.should == "application/pdf"
+      #  get :readpdf
+        
+
+
+	   end
+
+
+    end
+
 end
