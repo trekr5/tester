c52f24f4f2244ffd43eec752f6f2947f9d99c797
Angela Ebirim <cebirim@gmail.com>
2014-05-18 12:36:18 +0100

    whitepaper feature

--- app/controllers/whitepaper_controller.rb
+++ app/controllers/whitepaper_controller.rb
@@ -10,5 +10,10 @@ class WhitepaperController < ApplicationController
             :type => "application/pdf", 
             #:disposition => 'inline'
             :filename => "test.pdf"
+
+    send_file "public/test1.pdf", 
+            :type => "application/pdf", 
+            #:disposition => 'inline'
+            :filename => "test1.pdf"        
   end  
 end
