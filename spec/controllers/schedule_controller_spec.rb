34791cb7e0a179ac3fedfd26b8390d3d247e0cce
Angela Ebirim <cebirim@gmail.com>
2014-05-18 18:07:53 +0100

    schedule controller test

--- spec/controllers/schedule_controller_spec.rb
+++ spec/controllers/schedule_controller_spec.rb
@@ -2,10 +2,21 @@ require 'spec_helper'
 
 describe ScheduleController do
 
-  it "renders the :index view" do
+   describe "#index" do
+     it "populates a display of talks" do
+       talk = FactoryGirl.create(:talk)
+       get :index
+       assigns(:talks).should eq([talk])
+
+
+	end	
+
+  it "should show the index view" do
   get :index 
 
   	response.should render_template :index
    
+  end
+
   end	
 end
---
+++ spec/factories/talk.rb
@@ -0,0 +1,14 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :talk do
+    title "MyTalk"
+    description "This is a talk"
+    category "Introduction"
+    speaker "Mr Blobby"
+    image "/assets/baseball_player_vintage.jpg"
+    start_time 1030
+    end_time 1130
+
+  end
+end
